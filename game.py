5504cebe7e0d1fb510da0ef14f7e0ae5248f585f
Erlend.Skutlaberg <erlend.skutlaberg@student.uib.no>
2022-10-02 21:01:47 +0200

    Bug free, ish...

--- game.py
+++ game.py
@@ -20,10 +20,10 @@ def put(position: str, gamePiece: str):
     if (not len(position) == 2):
         return False
 
-    letter = position[0]
+    letter = position[0].upper()
     number = position[1]
-    pos1 = 1
-    pos2 = 1
+    pos1 = -1
+    pos2 = -1
     if (letter == "A"):
         pos1 = 0
 
@@ -38,54 +38,67 @@ def put(position: str, gamePiece: str):
         pos2 = 1
     elif (number == "3"):
         pos2 = 2
+
+    if (pos1 == -1) or (pos2 == -1):
+        return False
+    
     if board[pos1][pos2] == " ":
         board[pos1][pos2] = gamePiece
-    return True
+        return True
+    return False
+
+    
     
 def gameDone():
     win, piece = hWin()
     if win:
+        print("hWin")
         return piece
     win, piece = vWin()
     if win:
+        print("vWin")
         return piece
     win, piece = dWin()
     if win:
+        print("dWin")
         return piece
     return " "
 
 def hWin ():
     for row in board:
         piece = " "
+        count = 0
         for cell in row:
             if cell == " ":
                 break
             if piece == " ":
                 piece = cell
-            elif piece != cell:
-                break
-        return True, piece #win vist vi kommmer gjennom hele raden
+            if piece == cell:
+                count += 1
+        if (count == 3):
+            return True, piece #win vist vi kommmer gjennom hele raden
     return False, " "  #ingen win dersom om vi kommer hit
 
 def vWin ():
     for i in range(3):
         piece = " "
+        count = 0
         for j in range(3):
             cell = board[j][i]
             if cell == " ":
                 break
             if piece == " ":
                 piece = cell
-            elif piece != cell:
-                break
-        return True, piece #win vist vi kommmer gjennom hele raden
+            if piece == cell:
+                count += 1
+        if count == 3:
+            return True, piece #win vist vi kommmer gjennom hele raden
     return False, " "  #ingen win dersom om vi kommer hit
 
 def dWin ():
-    d1 = board[0][0]
     if board [0][0] == board [1][1] and board [0][0] == board[2][2] and board [0][0] != " ":
         return True, board[0][0]
-    if board [0][2] == board [1][1] and board [0][0] == board[2][0] and board [0][2] != " ":
+    if board [0][2] == board [1][1] and board [0][2] == board[2][0] and board [0][2] != " ":
         return True, board[0][2]
     return False, " "
 
@@ -94,6 +107,7 @@ p2 = "O"
 turn = p1            
 isDone = False
 while not isDone:
+    printBoard()
     pos = input(f"{turn}'s turn. Pick a position: ")
     if not put(pos, turn):
         print("Error, not valid position. Pick again")
@@ -107,6 +121,9 @@ while not isDone:
         turn = p1
 
 
+print("\n \n \n ")
+print("CONGRATULATIONS!")
+printBoard()
 print(f"The winner is {winPiece}")
 
 
