83d2389a45a72e7954d4102d879777c22233bef7
Skutlis <skutlis98@gmail.com>
2022-10-02 19:05:52 +0200

    Update

--- game.py
+++ game.py
@@ -1,1 +1,19 @@
-print("Hello harald")
+
+
+board = [["", "", ""],
+        ["", "", ""],
+        ["", "", ""]]
+
+def printBoard(board: list):
+    pass
+
+
+
+def put(position: str, gamePiece: str):
+    if (not len(position) == 2):
+        return
+
+    letter = position[0]
+    number = position[1]
+    
+
